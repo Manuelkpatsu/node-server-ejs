0ed8647ab089a3f4bd16bc256302838d36b2f3b5
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-04-14 23:40:50 +0000

    Update deletion of products

--- controllers/admin.js
+++ controllers/admin.js
@@ -179,8 +179,8 @@ exports.postEditProduct = (req, res, next) => {
         });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const id = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const id = req.params.productId;
     Product.findById(id)
         .then(product => {
             if (!product) {
@@ -191,12 +191,10 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(() => {
             console.log('Product deleted!');
-            res.redirect('/admin/products');
+            res.status(200).json({ message: 'Success!' });
         })
         .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({ message: 'Deleting product failed.' });
         });
 }
 
